781695f33481c4aa549e1d644b63ddb256b247be
Filip Culibrk <filipculibrk98@gmail.com>
2023-01-25 23:00:48 +0100

    test

--- TESTGIT/TESTGIT/Controllers/HomeController.cs
+++ TESTGIT/TESTGIT/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace TESTGIT.Controllers
         public IActionResult Index()
         {
             string draganinString = "Cao Dragana";
-            string filipovString = "Cao Filipeee";
+            string filipovString = "Cao Filipeee OVO JE TEST IZMENA";
             ViewBag.fString = filipovString;
             return View();
         }
